22378568392c182c91119aaeb14a15b85223b88d
Deki97 <dejan97@libero.it>
2022-10-24 11:15:42 +0200

    scaffolding base

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        Ciao sono l'header
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
