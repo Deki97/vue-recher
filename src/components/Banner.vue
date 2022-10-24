2e23e9703554225774a129dc45201fa6ad8d943c
Deki97 <dejan97@libero.it>
2022-10-24 12:05:58 +0200

    header + banner done

---
+++ src/components/Banner.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="banner">
+        <div class="image-banner">
+            <img :src="banners[0].image" :alt="banners[0].bannerTitle">
+
+            <div class="overlay"></div>
+
+            <div class="text-content">
+                <div class="banner-title">
+                    <h2>{{ banners[0].bannerTitle }}</h2>
+                </div>
+                <div class="banner-subtitle">
+                    <h4>{{ banners[0].bannerSubtitle }}</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Banner',
+    data: function() {
+        return {
+            banners: [
+                {
+                    image: require('../assets/img/banner1.png'),
+                    bannerTitle: 'Il nostro team',
+                    bannerSubtitle: 'Competenza e disponibilità a servizio del Cliente'
+                },
+                {
+                    image: require('../assets/img/banner2.png'),
+                    bannerTitle: 'Con il cliente, per il cliente',
+                    bannerSubtitle: 'La nostra esperienza è la soluzione per ogni tuo problema e la risposta per le tue esigenze'
+                },
+                {
+                    image: require('../assets/img/banner3.png'),
+                    bannerTitle: 'La nostra sede',
+                    bannerSubtitle: 'Palazzo delle Professioni 3^ Piano - Zanè (VI), via del Costo, n.10'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.banner {
+    height: calc(100vh - 130px);
+    background-color: pink;
+
+    .image-banner {
+        width: 100%;
+        height: 100%;
+        position: relative;
+
+        img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+
+        .overlay {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.2);
+        }
+
+        .text-content {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            color: white;
+            text-align: center;
+
+            .banner-title {
+                h2 {
+                    font-size: 4rem;
+                    letter-spacing: -1px;
+                    text-transform: uppercase;
+                }
+            }
+
+            .banner-subtitle {
+                h4 {
+                    font-size: 2.2rem;
+                }
+            }
+        }
+    }
+}
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,89 @@
 <template>
     <header>
-        Ciao sono l'header
+        <div class="container">
+            <div class="header-wrapper d-flex align-items-center justify-content-between">
+                <div class="header-left">
+                    <div class="logo">
+                        <img src="../assets/img/logo.png" alt="Recher Logo">
+                    </div>
+                </div>
+                <div class="header-right">
+                    <div class="menu">
+                        <nav>
+                            <ul class="d-flex">
+                                <li v-for="(menu, index) in menus" :key="index">
+                                    <a :href="menu.menuUrl">{{ menu.menuName }}</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
     </header>
 </template>
 
 <script>
 export default {
-    name: 'Header'
+    name: 'Header',
+    data: function() {
+        return {
+            menus: [
+                {
+                    menuName: 'Homepage',
+                    menuUrl: '#'
+                },
+                {
+                    menuName: 'Lo Studio',
+                    menuUrl: '#'
+                },
+                {
+                    menuName: 'Le attività',
+                    menuUrl: '#'
+                },
+                {
+                    menuName: 'Dove siamo',
+                    menuUrl: '#'
+                },
+                {
+                    menuName: 'Contatti',
+                    menuUrl: '#'
+                },
+                {
+                    menuName: 'News',
+                    menuUrl: '#'
+                },
+            ],
+
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+@import '../style/general.scss';
+@import '../style/variables.scss';
 
+header {
+    padding: 20px 0;
+
+    .header-right {
+        .menu {
+            nav {
+                ul {
+                    li {
+                        margin-left: 24px;
+                        font-size: 1.25rem;
+                        text-transform: uppercase;
+                        font-weight: 600;
+
+                        a {
+                            color: $page_primary_color;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 </style>
