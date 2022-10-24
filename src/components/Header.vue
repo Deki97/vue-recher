3ab584cc0ed94516b3cfd32dba8dede25cafcf8b
Deki97 <dejan97@libero.it>
2022-10-24 17:12:07 +0200

    responsive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -19,23 +19,6 @@
                     </div>
                 </div>
             </div>
-
-
-            <!-- <nav class="navbar navbar-expand-lg navbar-light d-flex justify-content-between">
-                <a class="navbar-brand" href="#">
-                    <img src="../assets/img/logo.png" alt="Recher Logo">
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                    <a class="nav-item nav-link active" href="#">Home</a>
-                    <a class="nav-item nav-link" href="#">Features</a>
-                    <a class="nav-item nav-link" href="#">Pricing</a>
-                    </div>
-                </div>
-            </nav> -->
         </div>
     </header>
 </template>
